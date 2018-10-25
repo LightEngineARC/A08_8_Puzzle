fb72543f5cf34cd3a6b00968f1deb56685167382
Ashton Chatelain <34180527+LightEngineARC@users.noreply.github.com>
2018-10-25 13:46:29 -0600

    implement several methods with testing in the test client at the bottom

--- src/puzzle/Board.java
+++ src/puzzle/Board.java
@@ -14,7 +14,6 @@ public class Board
 	 */
 	public Board(int[][] blocks)
 	{
-		// TODO implement constructor
 		this.N = blocks.length;
 		board = new int[N*N];
 		int row = 0;
@@ -40,12 +39,16 @@ public class Board
 		return 0;
 	}
 
-	
-	//TODO create n*n boards
 
 	public boolean isGoal() {
-		Board goalBoard;//TODO create a goal board for size N
-		return false; //this.equals(goalBoard);
+		for(int i=0;i<this.size()*this.size();i++) {
+			if(board[i]!=0) {
+			if(board[i]!=i+1) {
+				return false;
+			}
+			}
+		}
+		return true;
 	}
 	
 	public boolean isSolvable()
@@ -54,20 +57,24 @@ public class Board
 		return false;
 	}
 	
-	
 	public boolean equals(Board b) {
-		return false; //return this.equals(b);
+		if(this.size()!=b.size())
+			return false;
+		int[] thatBoard = b.getBoard();
+		for(int i= 0;i<board.length;i++) {
+			if(this.board[i] != thatBoard[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
-
-	
-
 
 
 	public int tileAt(int row, int col)
 	{
-		// TODO implement
-		return 0;
+		return board[row*N + col];
 	}
+	
 	/**
 	 * 
 	 * @see java.lang.Object#toString()
@@ -75,7 +82,7 @@ public class Board
 	public String toString() {
 		//TODO update this implementation
 	    StringBuilder s = new StringBuilder();
-//	    s.append(N + "\n");
+	    s.append(N + "\n");
 //	    for (int i = 0; i < N; i++) {
 //	        for (int j = 0; j < N; j++) {
 //	            s.append(String.format("%2d ", tiles[i][j]));
@@ -93,23 +100,25 @@ public class Board
 	}
 	
 	
-	
-	// TEST CLIENT
+	// =============================================================
+	// ++++++++++++++++     TEST CLIENT     ++++++++++++++++++++++++
+	// =============================================================
 	public static void main(String[] args) {
 		int[][] array = {{1,2,3},{4,5,6},{7,8,0}};
 		Board board = new Board(array);
-		
+		assert(board.tileAt(0, 0)==1);
+		assert(board.tileAt(0, 1)==2);
+		assert(board.tileAt(0, 2)==3);
+		assert(board.tileAt(1, 0)==4);
+		assert(board.tileAt(1,1)==5);
+		assert(board.tileAt(1,2)==6);
+		assert(board.tileAt(2, 0)==7);
+		assert(board.tileAt(2, 1)==8);
+		assert(board.tileAt(2, 2)==0);
 		testPrint(board);
-		
-		int[][] array2 = {
-				{1,2,3,4},
-				{5,6,7,8},
-				{9,10,11,12},
-				{13,14,15,0}
-		};
+		int[][] array2 = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{14,13,15,0}};
 		Board board2 = new Board(array2);
 		testPrint(board2);
-		
 		
 		
 	}
@@ -117,9 +126,12 @@ public class Board
 		System.out.println(b.getSize());
 		
 		for(int i = 0;i<b.getSize()*b.getSize();i++) {
-			System.out.print(" "+b.getBoard()[i]+" ");
+			System.out.print(""+b.getBoard()[i]+" ");
+			if((i+1)%b.size()==0) {
+				System.out.println();
+			}
 		}
-		System.out.println("\n");
-		
+		System.out.println("Is goal? "+b.isGoal());
+		System.out.println("tile at N,N: " +b.tileAt(b.getSize()-1, b.getSize()-1)+"\n");
 	}
 }
